b0494ad4400685174725235c8fae32a764b0aad4
DavidMartinezMerencioAlixar <alum.dmartinezm.2022@iesalixar.org>
2023-04-19 18:42:12 +0200

    Removed backend views and added routing

--- src-api/routes/parties.js
+++ src-api/routes/parties.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('user location');
+  res.send('party location');
 });
 
 module.exports = router;
